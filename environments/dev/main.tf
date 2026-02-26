9b65451d74ebc04d5b7bff6aa63b14dba8570d18
Lakshit Tyagi <lakshit175@gmail.com>
2026-02-26 16:14:50 +0530

    terraform fmt

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,13 +1,13 @@
 module "networking" {
   source = "../../modules/networking"
 
-  project_name            = var.project_name
-  environment             = var.environment
-  owner                   = var.owner
-  instance_tenancy        = var.instance_tenancy
-  cidr_block              = var.cidr_block
-  subnet_cidr_block       = var.subnet_cidr_block
-  availability_zone       = var.availability_zone
+  project_name      = var.project_name
+  environment       = var.environment
+  owner             = var.owner
+  instance_tenancy  = var.instance_tenancy
+  cidr_block        = var.cidr_block
+  subnet_cidr_block = var.subnet_cidr_block
+  availability_zone = var.availability_zone
 }
 
 module "compute" {
--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -11,9 +11,9 @@ resource "aws_vpc" "main" {
 
 # tfsec:ignore:aws-ec2-no-public-ip-subnet
 resource "aws_subnet" "main" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.subnet_cidr_block
-  availability_zone = var.availability_zone
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.subnet_cidr_block
+  availability_zone       = var.availability_zone
   map_public_ip_on_launch = true
 
   tags = {
